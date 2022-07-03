e857d337f35c639d4b30a2f75c63d9b3d29add89
asksowhat <srcrs@foxmail.com>
2022-07-03 10:50:39 +0200

    完成页面原型设置

---
+++ html/pages/base.js
@@ -0,0 +1,12 @@
+(function() {
+	const response = {
+		data: {
+			type: "page",
+			title: "基本设置",
+			body: "基本设置"
+		},
+		status: 0
+	}
+
+	window.jsonpCallback && window.jsonpCallback(response);
+})();
